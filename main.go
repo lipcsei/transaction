41e2fd7f1131e7523233155e572faeb048f43451
Sándor Lipcsei <sandor.lipcsei@gmail.com>
2016-09-21 16:25:49 +0200

    join

--- main.go
+++ main.go
@@ -1,68 +1,94 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"strconv"
+	"time"
 
 	"github.com/foosio/api/lib/services/db"
-	"github.com/foosio/api/lib/services/env"
 	"github.com/wawandco/fako"
 	"gopkg.in/mgo.v2/bson"
-	redis "gopkg.in/redis.v4"
 )
 
 var (
 	mongo *db.Mongo
 )
 
-const LIMIT = 100
+const LIMIT = 1000000
 
 type (
 	// User struct represent a User
 	User struct {
 		ID   string `json:"id" bson:"_id,omitempty"`
 		Name string `json:"name" bson:"name" fako:"full_name"`
+		PID  int    `json:"pid" bson:"pid"`
 	}
 
 	// Game struct represent a Game
 	Game struct {
-		ID    string `json:"id" bson:"_id,omitempty"`
-		Size  int    `json:"size" bson:"size"`
-		Users []User `json:"users" bson:"users"`
+		ID          string    `json:"id" bson:"_id,omitempty"`
+		Size        int       `json:"size" bson:"size"`
+		Users       []User    `json:"users" bson:"users"`
+		LastUpdated time.Time `json:"lastUpdated" bson:"lastUpdated"`
 	}
 )
 
-func Join(gameID string, userID int) {
+func Join(gameID string, userID int) error {
 	collection, session := mongo.GetCollection("transaction")
 	defer session.Close()
-	client := ConnectToRedis()
-	defer client.Close()
 
 	user := User{ID: strconv.Itoa(userID + 1)}
 	fako.Fill(&user)
+	user.PID = os.Getppid()
 
-	collection.Update(bson.M{"_id": gameID}, bson.M{"$push": bson.M{"users": &user}})
+	var game Game
+	err := collection.Find(bson.M{"_id": gameID}).One(&game)
+
+	if err != nil {
+		fmt.Println(gameID)
+		return err
+	}
+
+	err = collection.Update(bson.M{
+		"_id":         gameID,
+		"lastUpdated": game.LastUpdated,
+		"size":        bson.M{"$lt": LIMIT},
+	},
+		bson.M{
+			"$push":        bson.M{"users": &user},
+			"$currentDate": bson.M{"lastUpdated": true},
+			"$inc":         bson.M{"size": 1},
+		})
+
+	return err
 }
 
 func main() {
-
 	gameID := "1"
+	CreateGame(gameID)
+
+	success := 0
+	failed := 0
+	for userID := 0; userID < LIMIT; userID++ {
+		err := Join(gameID, userID)
+		if err != nil {
+			failed++
+			fmt.Println(err)
+			// break
+		} else {
+			success++
+		}
+	}
+
+	fmt.Println("success: ", success, "failed: ", failed, os.Getpid())
+}
+
+func CreateGame(gameID string) {
 	collection, session := mongo.GetCollection("transaction")
 	defer session.Close()
 	collection.RemoveId(gameID)
-	game := Game{ID: gameID, Size: LIMIT}
+	game := Game{ID: gameID, Size: 0, LastUpdated: time.Now()}
 	collection.Insert(&game)
 
-	for userID := 0; userID < LIMIT; userID++ {
-		Join(gameID, userID)
-	}
-
-}
-
-func ConnectToRedis() *redis.Client {
-	client := redis.NewClient(&redis.Options{
-		Addr:     env.Get("REDIS_HOST", "localhost:6379"),
-		Password: "",
-		DB:       0,
-	})
-	return client
 }
